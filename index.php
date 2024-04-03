f1d21cf3f5464aa162d5f62f0da30baa8e98423a
Proyecto Madagascar Films <145238589+MadagascarFilms@users.noreply.github.com>
2024-04-03 17:25:39 -0500

    Routes

--- index.php
+++ index.php
@@ -7,40 +7,40 @@
     error_log('Inicio de la aplicacion');
 
     //Incluimos algunos archivos
-    require_once 'vendor/autoload.php';
+    require_once APP_URL.'vendor/autoload.php';
 
-    require_once 'libs/api.php'; 
-    require_once 'php/utils.php';
-    require_once 'classes/session.php';
-    require_once 'libs/controller.php';
-    require_once 'classes/sessionController.php';
-    require_once 'classes/infomensajes.php';
-    require_once 'classes/errormensajes.php';
-    require_once 'classes/exitomensajes.php';
-    require_once 'classes/alertas.php';
+    require_once APP_URL.'$libs/api.php'; 
+    require_once APP_URL.'php/utils.php';
+    require_once APP_URL.'classes/session.php';
+    require_once APP_URL.'libs/controller.php';
+    require_once APP_URL.'classes/sessionController.php';
+    require_once APP_URL.'classes/infomensajes.php';
+    require_once APP_URL.'classes/errormensajes.php';
+    require_once APP_URL.'classes/exitomensajes.php';
+    require_once APP_URL.'classes/alertas.php';
    
-    require_once 'libs/model.php';
-    require_once 'libs/view.php';
-    require_once 'libs/app.php';
-    require_once 'libs/IModel.php';
+    require_once APP_URL.'libs/model.php';
+    require_once APP_URL.'libs/view.php';
+    require_once APP_URL.'libs/app.php';
+    require_once APP_URL.'libs/IModel.php';
 
-    require_once 'config/config.php';
+    require_once APP_URL.'config/config.php';
 
     //Incluimos los modelos y controladores
-    require_once 'app/models/usuarioModel.php';
-    require_once 'app/models/categoriaModel.php';
-    require_once 'app/models/marcaModel.php';
-    require_once 'app/models/citaAdminModel.php';
-    require_once 'app/controllers/categoria.php';
-    require_once 'app/controllers/marca.php';
-    require_once 'app/controllers/citaAdmin.php';
-    require_once 'app/models/usuarioModel.php';
-    require_once 'app/controllers/usuario.php';
-    require_once 'app/models/productoModel.php';
-    require_once 'app/controllers/producto.php';
-    require_once 'app/models/proveedorModel.php';
-    require_once 'app/controllers/proveedor.php';
-    require_once 'app/models/servicioModel.php';
+    require_once APP_URL.'app/models/usuarioModel.php';
+    require_once APP_URL.'app/models/categoriaModel.php';
+    require_once APP_URL.'app/models/marcaModel.php';
+    require_once APP_URL.'app/models/citaAdminModel.php';
+    require_once APP_URL.'app/controllers/categoria.php';
+    require_once APP_URL.'app/controllers/marca.php';
+    require_once APP_URL.'app/controllers/citaAdmin.php';
+    require_once APP_URL.'app/models/usuarioModel.php';
+    require_once APP_URL.'app/controllers/usuario.php';
+    require_once APP_URL.'app/models/productoModel.php';
+    require_once APP_URL.'app/controllers/producto.php';
+    require_once APP_URL.'app/models/proveedorModel.php';
+    require_once APP_URL.'app/controllers/proveedor.php';
+    require_once APP_URL.'app/models/servicioModel.php';
 
 
     //Iniciamos la aplicacion GO!!
